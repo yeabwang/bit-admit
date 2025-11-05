34fa549f8669179f42560b2cd9756f0cfc58134e
Ye'ab <yeabsiratesfaye781@gmail.com>
2025-11-05 11:05:52 +0000

    mongo client documented

--- BIT_ADMIT_AI/configration/mongo_connect.py
+++ BIT_ADMIT_AI/configration/mongo_connect.py
@@ -1,3 +1,11 @@
+"""MongoDB connection utility.
+
+Provides MongoDbClient, a thin wrapper around a shared pymongo.MongoClient.
+- Reads connection info from SystemConfig (MONGODB_URL_KEY, DATABASE_NAME).
+- Uses certifi CA bundle for TLS.
+- Raises BitAdmitAIException on misconfiguration or connection failures.
+"""
+
 import sys
 import os
 import pymongo
@@ -10,11 +18,28 @@ ca = certifi.where()
 
 
 class MongoDbClient:
-    """Creating a mongo db connection, uses the system config"""
+    """Create and manage a shared MongoDB client connection.
+
+    Attributes:
+        client: shared pymongo.MongoClient instance.
+        database:  Database object for SystemConfig.DATABASE_NAME.
+        database_name: Name of the active database.
+    """
 
     client = None
 
     def __init__(self, system_config: SystemConfig):
+        """Initialize the MongoDB client from SystemConfig.
+
+        Creates a singleton pymongo.MongoClient and
+        binds the configured database.
+
+        Args:
+            system_config: Provides MONGODB_URL_KEY and DATABASE_NAME.
+
+        Raises:
+            BitAdmitAIException: If MONGODB_URL_KEY is missing/empty or connection fails.
+        """
         try:
             if not MongoDbClient.client:
                 self.mongo_db_url = system_config.MONGODB_URL_KEY
