9733b3242a5bbae07c470cd789b01780fb181fad
Ye'ab <yeabsiratesfaye781@gmail.com>
2025-11-05 10:34:52 +0000

    mongo client documented

--- BIT_ADMIT_AI/data_access/data_access.py
+++ BIT_ADMIT_AI/data_access/data_access.py
@@ -1,3 +1,11 @@
+"""MongoDB data access utilities.
+
+Provides DataAccessAndHandling to pull MongoDB collections into pandas DataFrames.
+- Connects via MongoDbClient using SystemConfig.
+- Drops MongoDB "_id" and normalizes "na" to NaN.
+- Raises BitAdmitAIException on failures.
+"""
+
 import pandas as pd
 import numpy as np
 import sys
@@ -9,6 +17,16 @@ from BIT_ADMIT_AI.exceptions import BitAdmitAIException
 
 
 class DataAccessAndHandling:
+    """Thin wrapper over MongoDbClient to read collections as DataFrames.
+
+    Args:
+        system_config: Configuration providing DATABASE_NAME and connection params.
+
+    Attributes:
+        database_name: Default database name used when not overridden.
+        mongo_client: Connected MongoDbClient instance.
+    """
+
     def __init__(self, system_config: SystemConfig = SystemConfig()):
         self.database_name = system_config.DATABASE_NAME
 
@@ -20,7 +38,20 @@ class DataAccessAndHandling:
     def collection_to_dataframe(
         self, collection_name: str, database_name: Optional[str] = None
     ) -> pd.DataFrame:
-        """For flexability allowing to pass db name with fallback to the default"""
+        """Load a MongoDB collection into a pandas DataFrame.
+
+        Drops "_id" if present and replaces string "na" with NaN.
+
+        Args:
+            collection_name: Name of the collection to read.
+            database_name: Optional database override; defaults to the configured database.
+
+        Returns:
+            pandas.DataFrame: Collection data.
+
+        Raises:
+            BitAdmitAIException: On connection or read errors.
+        """
         try:
             target_db = database_name or self.database_name
             collection = self.mongo_client.client[target_db][collection_name]
